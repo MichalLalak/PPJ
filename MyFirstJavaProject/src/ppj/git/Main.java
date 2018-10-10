e3c050ddb05b1dc1b27b5f2c6750530f459ba54d
Michal <mlalak8@wp.pl>
2018-10-10 17:17:04 +0200

    Moj pierwszy program zapisany w Git

---
+++ MyFirstJavaProject/src/ppj/git/Main.java
@@ -0,0 +1,10 @@
+package ppj.git;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("My first java project with git");
+	}
+
+}
